1a5c57c04e910202024b09db9f62ba84ea6c9354
Rachel Shields <rjs318@lehigh.edu>
2015-01-24 23:31:15 +0000

    code for homework one

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,18 @@
+//Rachel Shields
+//Homework 1
+// 1-23-15
+public class WelcomeClass{
+    public static void main(String[] args){
+        System.out.println("  ----------- ");
+        System.out.println("  | WELCOME | ");
+        System.out.println("  ----------- ");
+        System.out.println("  ^  ^  ^  ^  ^  ^ ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-R--J--S--3--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("Over winter break, I watched six whole seasons of");
+        System.out.println("The Office and I slept in until twelve everyday!");
+    }
+}
+        
