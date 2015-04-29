d52ebf4c9428531b139818cd9fa8c0b9f69e7ba6
Rachel Shields <rjs318@lehigh.edu>
2015-04-29 18:21:44 +0000

    updated hw13

--- hw13/FourDwin.java
+++ hw13/FourDwin.java
@@ -59,47 +59,132 @@ public class FourDwin{
         output=("Members: "+elements+"\n"+"Sum: "+sum+"\n"+"Mean: "+mean);
         return output;
     }//end of statArray method
-    /* sort4DArray method needs to use insertion sort to compare the sizes of all the 3D arrays in the 4D
-    array and then rearrange them so that the sizes are in ascending order. if the sizes of any of the 3D
-    arrays are the same, they are sorted according to the values of the 3D array through a selection sort.
-    I was unable to write the methods however I included an attempt in comments
-    
+    /*sort4DArray uses insertion sort to compare the sizes of all the 3D arrays in the 4D
+    array and then rearrange them so that the sizes are in ascending order. After the 3D arrays are sorted by size,
+    their values are sorted by calling the 3D sort method. At the end of the sort4D method, the method countlength is called.
+    */
     public static void sort4DArray(double[][][][] A) {
-        int minlength=0; //length of smallest 3D array
-        int k=0;
-        for(int i=0; i<A.length;i++){
-            for (int j=1; j<A[i].length; j++) { //j is equal to one because the first posiition itself is sorted
-                minlength = A[j].length; 
-                k = j;
-                while (k >0 &&A[k - 1].length > minlength) {//should shift 3D array back in its order until 
-                //it is sorted
-                      A[i]= A[k - 1][][];// somehow need to move the order within the 4D array but I don't know how
-                      k--;
-                }
-                minlength=A[i].length;
-          }
+        //insertion sort that sorts 4D array from smallest to largest 3D arrays
+        double temp[][][];
+        int j=0;
+        for(int i=1;i<A.length;i++){
+            temp = A[i];
+            j=(i-1);
+            while(j>=0&&(temp.length<A[j].length)){
+                A[j+1]=A[j];
+                j--;
+            }
+            A[j+1]=temp;
         }
-        //need something to check if any of the 3D arrays are the same size, if they are - call 
-        sort3Darray within the method
+        //sorts all the 3D arrays
+        for(int m=0; m<A.length;m++){
+                sort3Darray(A[m]);
+          }
+       
+        countLength(A);
     }
-    public static void sort3Darray(double[] A){
-        //uses selection sort to sort the values within the 3D array as a 1D array and then
-        //somehow should transfer them back into the 4Darray 
-        //then somehow puts them into the 4D array as sorted
+    //the countLength method checks if an of the 3D arrays have the same length, 
+    //if they do, the method rearranges them in order of lowest value in 3D array
+    public static void countLength(double[][][][]A){
+        double temp[][][];
+        int zero=0;
+        int one=1;
+        int two=2;
+        double lowest1=A[0][0][0][0];
+        double lowest2=A[1][0][0][0];
+        double lowest3=A[2][0][0][0];
+        //finds lowest value in first 3D array
+        for(int i=0; i<1; i++){
+            for(int j=0; j<A[i].length; j++){
+                for(int k=0; k<A[i][j].length; k++){
+                    for(int l=0; l<A[i][j][k].length;l++){
+                        if(A[i][j][k][l]<lowest1){
+                            lowest1=A[i][j][k][l];
+                        }
+                    }
+                }
+            }
+        }
+        //finds lowest value in second 3D array
+        for(int i=1; i<2; i++){
+            for(int j=0; j<A[i].length; j++){
+                for(int k=0; k<A[i][j].length; k++){
+                    for(int l=0; l<A[i][j][k].length;l++){
+                        if(A[i][j][k][l]<lowest1){
+                            lowest2=A[i][j][k][l];
+                        }
+                    }
+                }
+            }
+        }
+        //finds lowest value in third 3D array
+       for(int i=2; i<3; i++){
+            for(int j=0; j<A[i].length; j++){
+                for(int k=0; k<A[i][j].length; k++){
+                    for(int l=0; l<A[i][j][k].length;l++){
+                        if(A[i][j][k][l]<lowest1){
+                            lowest3=A[i][j][k][l];
+                        }
+                    }
+                }
+            }
+        }
+    //compares the lengths of the values and swaps them according to their lowest values if the lengths are equal    
+    if(A[zero].length==A[one].length){
+        if(lowest2<lowest1){
+            temp=A[zero];
+            A[zero]=A[one];
+            A[one]=temp;
+        }
+    }
+    if(A[zero].length==A[two].length){
+        if(lowest3<lowest1){
+            temp=A[zero];
+            A[zero]=A[two];
+            A[two]=temp;
+        }
+    }
+    if(A[one].length==A[two].length){
+        if(lowest3<lowest2){
+            temp=A[one];
+            A[one]=A[two];
+            A[two]=temp;
+        }
+    }
+}
+    
+    public static void sort3Darray(double[][][] A){
+        //sorts the values in a 3D array by filling a 1D array with those values and calling selection sort on the 
+        //1D array. 
+        for(int i=0; i<A.length; i++){
+            for(int j=0;j<A[i].length;j++){
+                double[] B = new double[A[i][j].length];
+                for(int k=0; k<A[i][j].length;k++){
+                    B[k]=A[i][j][k];
+                }
+                SelectionSort(B);
+                for(int l=0; l<A[i][j].length;l++){//puts sorted values of 1D array into the original 3D array
+                    A[i][j][l]=B[l];
+                }
+                
+            }    
+        }
+    }//end of sort3Darray
+    public static void SelectionSort(double[] A){
+        //sorts values in ascending order
         int first=0;
-        int temp=0;  
+        double temp=0;  
         for (int i= A.length-1;i>0;i--)  {
             first = 0;
-            for(int j=1;j<= i;j++){
+            for(int j=1;j<=i;j++){
                 if( A[j]>A[first] )         
                     first = j;
           }
           temp= A[first];
           A[first] = A[i];
           A[i]= temp; 
-      }           
+        }
     }
-    */
     
     public static void main(String[] args){
         //prompts user for two integers X and Y where Y>X
@@ -144,9 +229,9 @@ public class FourDwin{
         }    
         System.out.println("Original Array: ");
         System.out.println(printArray(four));
-        //System.out.println("Sorted Array: ");
-        //sort4DArray(four);
-        //System.out.println(printArray(four));
+        System.out.println("Sorted Array: ");
+        sort4DArray(four);
+        System.out.println(printArray(four));
         System.out.println(statArray(four));
         
     }//end of main method
